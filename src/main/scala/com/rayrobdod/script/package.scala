9953e6385f4b80fa2285fd672f34faa8a9e83501
Raymond Dodge <git@rayrobdod.name>
2015-04-14 00:21:11 -0400

    Giving constTrue a doccomment

--- src/main/scala/com/rayrobdod/script/package.scala
+++ src/main/scala/com/rayrobdod/script/package.scala
@@ -6,7 +6,7 @@ import scala.runtime.{AbstractFunction1 => AFunction1}
 package object script {
 	
 	
-	
+	/** A function that always returns true */
 	object constTrue extends AFunction1[Any, Boolean] { 
 		def apply(a:Any) = true
 		override def toString = "constTrue"
